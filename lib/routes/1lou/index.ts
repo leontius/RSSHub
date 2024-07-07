58f647e658b290b38dfd12c075f8055d3ed91a40
Leon Zhang <leontius.zy@gmail.com>
2024-07-07 11:44:49 +0800

    Add detail result search.

--- lib/routes/1lou/index.ts
+++ lib/routes/1lou/index.ts
@@ -91,6 +91,20 @@ export const handler = async (ctx) => {
         )
     );
 
+    if (items.length === 0) {
+        items = $('ul.attachlist li a')
+            .toArray()
+            .map((item) => {
+                const a = $(item);
+                return {
+                    title: a.text(),
+                    link: `${rootUrl}/${a.attr('href')}`,
+                    enclosure_type: 'application/x-bittorrent',
+                    enclosure_url: `${rootUrl}/${a.attr('href')}`,
+                };
+            });
+    }
+
     const author = 'BT 之家 1LOU 站';
     const image = new URL($('img.logo-2').prop('src'), rootUrl).href;
 
@@ -118,9 +132,9 @@ export const route: Route = {
   \`1lou.me/\` 后的内容填入 params 参数，以下是几个例子：
 
   若订阅 [大陆电视剧](https://www.1lou.me/forum-2-1.htm?tagids=0_97_0_0)，网址为 \`https://www.1lou.me/forum-2-1.htm?tagids=0_97_0_0\`。截取 \`https://www.1lou.me/\` 到末尾 \`.htm\` 的部分 \`forum-2-1\` 作为参数，并补充 \`tagids\`，此时路由为 [\`/1lou/forum-2-1?tagids=0_97_0_0\`](https://rsshub.app/1lou/forum-2-1?tagids=0_97_0_0)。
-  
+
   若订阅 [最新发帖电视剧](https://www.1lou.me/forum-2-1.htm?orderby=tid&digest=0)，网址为 \`https://www.1lou.me/forum-2-1.htm?orderby=tid&digest=0\`。截取 \`https://www.1lou.me/\` 到末尾 \`.htm\` 的部分 \`forum-2-1\` 作为参数，并补充 \`orderby\`，此时路由为 [\`/1lou/forum-2-1?orderby=tid\`](https://rsshub.app/1lou/forum-2-1?orderby=tid)。
-  
+
   若订阅 [搜素繁花主题贴](https://www.1lou.me/search-_E7_B9_81_E8_8A_B1-1.htm)，网址为 \`https://www.1lou.me/search-_E7_B9_81_E8_8A_B1-1.htm\`。截取 \`https://www.1lou.me/\` 到末尾 \`.htm\` 的部分 \`search-_E7_B9_81_E8_8A_B1-1\` 作为参数，此时路由为 [\`/1lou/search-_E7_B9_81_E8_8A_B1-1\`](https://rsshub.app/1lou/search-_E7_B9_81_E8_8A_B1-1)。
   :::`,
     categories: ['multimedia'],
